6365ff4dcd670ed574da9c182180f81cd1ed721a
set <set>
2021-04-16 16:25:49 +0200

    Add: Insta buttons w/ styling

--- src/Button.tsx
+++ src/Button.tsx
@@ -8,3 +8,7 @@ export const Button = kurasu.button(buttonClass);
 export const blueButtonClass =
   "bg-blue-300 inline-flex items-center p-2 justify-center rounded border-blue-400 border";
 export const BlueButton = kurasu.button(blueButtonClass);
+
+export const pinkButtonClass =
+  "bg-pink-200 inline-flex items-center p-2 justify-center rounded border-pink-300 border";
+export const PinkButton = kurasu.button(pinkButtonClass);
